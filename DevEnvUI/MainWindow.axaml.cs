634d4d09192c347d0d2614493184e621dc055cc6
William Dawson <william@wtdawson.info>
2025-12-10 14:03:19 +0000

    Started to add the UI

---
+++ DevEnvUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DevEnvUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
